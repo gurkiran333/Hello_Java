a11cb6707c6322db52b8e9e1028f3cf179e3784a
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2024-07-26 01:28:29 +0530

    Code1 file added

---
+++ Code1.java
@@ -0,0 +1,18 @@
+public class AriOperator {
+
+public static void  main(String []args) {
+
+int a = 23;
+int b = 33;
+
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+System.out.println(++a);
+System.out.println(--a);
+
+  }
+}
+    
